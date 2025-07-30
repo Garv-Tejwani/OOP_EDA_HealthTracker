db079af14fafbd11b6ecb4f4ec99293f0c52b5ab
Garv Tejwani <garvtejwani@Garvs-MacBook-Air.local>
2025-07-30 13:14:27 +0530

    Used classmethod and created separate csv files for each user

--- Main.py
+++ Main.py
@@ -5,18 +5,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-user = User('garv',18,'male')
+user1 = User('garv',18,'male')
+
+entries = [
+    HealthMetric("2025-07-15", 4500, 1900, 6.0, "Tired"),
+    HealthMetric("2025-07-16", 5200, 2100, 6.5, "Neutral"),
+    HealthMetric("2025-07-17", 8100, 2500, 7.5, "Happy"),
+    HealthMetric("2025-07-18", 3000, 1800, 5.0, "Low"),
+    HealthMetric("2025-07-19", 7000, 2300, 6.8, "Good"),
+    HealthMetric("2025-07-20", 6500, 2200, 7.0, "Neutral"),
+    HealthMetric("2025-07-21", 9000, 2600, 8.0, "Happy"),
+    HealthMetric("2025-07-22", 4000, 2000, 5.5, "Stressed"),
+    HealthMetric("2025-07-23", 7500, 2400, 7.2, "Good"),
+    HealthMetric("2025-07-24", 8300, 2550, 7.8, "Happy")
+]
 
 
 
-user_data_1= pd.read_csv('data/Health_Data.csv')
-user_data_1['entry_date']=pd.to_datetime(user_data_1['entry_date'])
 
-plt.plot(user_data_1['entry_date'],user_data_1['steps'],marker='o')
-plt.title('steps over time')
-plt.xlabel('entry_date')
-plt.ylabel('steps')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
+for entry in entries:
+    user1.add_entry(entry)
+user1.save_to_csv("data/Garv_Data.csv")
--- src/User.py
+++ src/User.py
@@ -1,10 +1,50 @@
+import pandas as pd 
+import csv
+from src.HealthMetric import HealthMetric
+
+
 class User:
     def __init__(self, name ,age, gender):
         self.name = name
         self.age = age
         self.gender = gender
+        self.health_data = []
+
+
     def info(self):
         print(f'Name :{self.name}')
         print(f'Age :{self.age}')
         print(f'Gender :{self.gender}')
 
+        
+    def add_entry(self , metric):
+        self.health_data.append(metric)
+
+
+
+    def get_data_as_dataframe(self):
+        import pandas as pd
+        df=pd.DataFrame([entry.self_dict() for entry in self.health_data])
+    
+    def save_to_csv(self, filename):
+        with open(filename , 'w', newline='') as f:
+            writer=csv.DictWriter(f ,fieldnames=['entry_date','steps','calories','sleep_hours','mood'])
+            writer.writeheader
+            for entry in self.health_data:
+                writer.writerow(entry.self_dict())
+    
+
+    @classmethod
+    def load_from_csv(cls, name , age, gender, filename):
+        user=cls(name , age , gender)
+        df=pd.read_csv(filename)
+
+        for _,row in df.iterrows:
+            metric=HealthMetric( row["date"],
+            row["steps"],
+            row["calories"],
+            row["sleep_hours"],
+            row["mood"])
+            
+            user.add_entry(metric)
+        return user 
